e827479042afee3b835996fae589d0fe6cc2e079
d9magai <kumagai@sencorp.co.jp>
2016-10-31 01:42:39 +0900

    add files

---
+++ src/module_config_struct.h
@@ -0,0 +1,12 @@
+/*
+ * module_config_struct.h
+ */
+
+#ifndef MODULE_CONFIG_STRUCT_H_
+#define MODULE_CONFIG_STRUCT_H_
+
+struct yacco_config {
+	std::shared_ptr<std::string> sha256secretkey;
+};
+
+#endif /* MODULE_CONFIG_STRUCT_H_ */
