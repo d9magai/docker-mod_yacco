40828f163e61af4786f28c0528429b0e1ce75955
d9magai <kumagai@sencorp.co.jp>
2016-11-01 10:23:52 +0000

    implements cached_s3client

---
+++ src/cached_s3client.cpp
@@ -0,0 +1,23 @@
+#include "cached_s3client.h"
+
+std::shared_ptr<Aws::S3::S3Client> getS3Client(request_rec *r)
+{
+
+    yacko_config *conf = reinterpret_cast<yacko_config*>(ap_get_module_config(r->server->module_config, &yacko_module));
+    if (conf->s3client) {
+        return conf->s3client;
+    }
+
+    Aws::Client::ClientConfiguration config;
+    config.scheme = Aws::Http::Scheme::HTTPS;
+    config.connectTimeoutMs = 30000;
+    config.requestTimeoutMs = 30000;
+    config.region = Aws::Region::AP_NORTHEAST_1;
+
+    conf->s3client = Aws::MakeShared<Aws::S3::S3Client>(ALLOCATION_TAG, Aws::Auth::AWSCredentials(*(conf->aws_accesskey_id), *(conf->aws_secretaccess_key)), config);
+    if(!(conf->s3client)) {
+        throw yacko::internal_server_error("init S3Client failed");
+    }
+    return conf->s3client;
+}
+
---
+++ src/cached_s3client.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <memory>
+#include <httpd.h>
+#undef OK
+#undef HTTP_VERSION_NOT_SUPPORTED
+#include "http_config.h"
+#include "module_config_struct.h"
+#include "serverexception.h"
+#include <aws/s3/S3Client.h>
+#include <aws/core/auth/AWSCredentialsProvider.h>
+
+extern "C" module AP_MODULE_DECLARE_DATA yacko_module;
+
+static const char* ALLOCATION_TAG = "yacko";
+
+extern "C" {
+std::shared_ptr<Aws::S3::S3Client> getS3Client(request_rec *r);
+}
+
