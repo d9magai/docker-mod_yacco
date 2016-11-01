1a838d204627c412313a13f6ae455b7fa2be7f5c
d9magai <kumagai@sencorp.co.jp>
2016-11-01 01:48:38 +0000

    use Aws::String

--- src/module_config_struct.h
+++ src/module_config_struct.h
@@ -5,11 +5,13 @@
 #ifndef MODULE_CONFIG_STRUCT_H_
 #define MODULE_CONFIG_STRUCT_H_
 
+#include <aws/core/utils/StringUtils.h>
+
 struct yacco_config
 {
     std::shared_ptr<std::string> sha256secretkey;
-    std::shared_ptr<std::string> aws_accesskey_id;
-    std::shared_ptr<std::string> aws_secretaccess_key;
+    std::shared_ptr<Aws::String> aws_accesskey_id;
+    std::shared_ptr<Aws::String> aws_secretaccess_key;
 };
 
 #endif /* MODULE_CONFIG_STRUCT_H_ */
