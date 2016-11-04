9b62e59857495ed35c9c5d8558d056eeec1e1330
d9magai <kumagai@sencorp.co.jp>
2016-11-05 02:04:03 +0900

    indent

--- src/yacko/serverexception.h
+++ src/yacko/serverexception.h
@@ -6,19 +6,23 @@
 
 namespace Yacko {
 
-class BAD_REQUEST: public std::runtime_error {
-public:
-    explicit BAD_REQUEST(const std::string& s) :
-            std::runtime_error(s) {
-    }
-};
+    class BAD_REQUEST: public std::runtime_error
+    {
+    public:
+        explicit BAD_REQUEST(const std::string& s)
+                : std::runtime_error(s)
+        {
+        }
+    };
 
-class INTERNAL_SERVER_ERROR: public std::runtime_error {
-public:
-    explicit INTERNAL_SERVER_ERROR(const std::string& s) :
-            std::runtime_error(s) {
-    }
-};
+    class INTERNAL_SERVER_ERROR: public std::runtime_error
+    {
+    public:
+        explicit INTERNAL_SERVER_ERROR(const std::string& s)
+                : std::runtime_error(s)
+        {
+        }
+    };
 
 }
 
