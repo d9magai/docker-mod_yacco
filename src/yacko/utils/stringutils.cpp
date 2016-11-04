6a29819fb23fd79dee6f7d667066b8bbcfd0e12f
d9magai <kumagai@sencorp.co.jp>
2016-11-04 12:18:24 +0000

    remove unnecessary function

--- src/yacko/utils/stringutils.cpp
+++ src/yacko/utils/stringutils.cpp
@@ -10,13 +10,6 @@ namespace Yacko {
             return ss.str();
         }
 
-        Aws::String buf2as(const char *buf)
-        {
-            Aws::StringStream ass;
-            ass << buf;
-            return ass.str();
-        }
-
         std::string as2s(const Aws::String& as)
         {
             std::stringstream ss;
--- src/yacko/utils/stringutils.h
+++ src/yacko/utils/stringutils.h
@@ -9,7 +9,6 @@ namespace Yacko {
     namespace Utils {
 
         std::string buf2s(std::basic_streambuf<char>* buf);
-        Aws::String buf2as(const char *buf);
         std::string as2s(const Aws::String& as);
         std::map<std::string, std::string> parseUri(std::string uri);
         std::map<std::string, std::string> parseArgs(std::string args);
