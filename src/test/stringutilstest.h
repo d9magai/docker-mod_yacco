64bedd472f481d31bc0712c7e0f95c0143781948
d9magai <kumagai@sencorp.co.jp>
2016-11-08 11:35:16 +0000

    w,h

--- src/test/stringutilstest.h
+++ src/test/stringutilstest.h
@@ -3,9 +3,11 @@
 
 TEST_CASE("parseUri Test", "[utils][stringutils]") {
 
-    std::map<std::string, std::string> map = Yacko::Utils::parseUri(std::string("/yacko/bucket/path/to/object"));
-    REQUIRE(map["bucket"] == "bucket");
-    REQUIRE(map["objectkey"] == "path/to/object");
+    std::map<std::string, std::string> map = Yacko::Utils::parseUri(std::string("/yacko(w=100,h=200)/bucket/path/to/object"));
+    CHECK(map["bucket"] == "bucket");
+    CHECK(map["objectkey"] == "path/to/object");
+    CHECK(map["w"] == "100");
+    CHECK(map["h"] == "200");
 }
 
 TEST_CASE("parseArgs Test", "[utils][stringutils]") {
