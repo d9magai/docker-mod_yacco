d25856fdb45c49e351ddeac31e66a5751cbb68fe
d9magai <kumagai@sencorp.co.jp>
2017-01-10 07:47:52 +0900

    use getOutputFormat in resize

--- src/yacko/image/convert.cpp
+++ src/yacko/image/convert.cpp
@@ -3,16 +3,17 @@
 namespace Yacko {
     namespace Image {
 
-        Magick::Blob resize(std::map<std::string, std::string> map, Magick::Image image)
+        Magick::Blob resize(std::map<std::string, std::string> map, Magick::Image image, std::string basetype)
         {
 
             int w = std::stoi(map["w"]);
             int h = std::stoi(map["h"]);
+            std::string of = Yacko::Image::getOutputFormat(map, basetype);
             Magick::Geometry newSize = Magick::Geometry(w, h);
             newSize.aspect(false);
             image.resize(newSize);
             Magick::Blob blob;
-            image.magick("JPEG");
+            image.magick(of);
             image.write(&blob);
             return blob;
         }
